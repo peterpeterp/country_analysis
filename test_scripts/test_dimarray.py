84813bbea658758200021b5ee2d9791b46287f06
Peter <peterpfleiderer@gmx.de>
2017-01-26 13:13:53 +0100

    possibility to use disarray as dict checked

--- test_scripts/test_dimarray.py
+++ test_scripts/test_dimarray.py
@@ -4,15 +4,28 @@ import numpy as np
 lat=range(10)
 lon=range(20)
 time=range(5)
-models=['mod1','mod2']
 data=np.zeros([5,10,20])
 
-test1=da.DimArray(axes=[np.asarray(models),time,lat,lon],dims=['model','time','lat','lon'])
-test1['mod1']=data
+models=['mod1','mod2','Na']
+scenarios=['rcp26','rcp85','Na']
+datasets=['ncep','cru','Na']
+
+test1=da.DimArray(axes=[np.asarray(models),np.asarray(scenarios),np.asarray(datasets),time,lat,lon],dims=['model','scenario','dataset','time','lat','lon'])
+test1['mod1']['rcp26']['Na']=data
+
+test1['Na']['Na']['cru']=data
+
+
+
+
+
+
+
+
 
 models2=['mod1','mod2','mod3']
 
-test2=da.DimArray(axes=[np.asarray(models2),time,lat,lon],dims=['model','time','lat','lon'])
+test2=da.DimArray(axes=[np.asarray(models2),np.asarray(models2),np.asarray(models2),time,lat,lon],dims=['model','scenario','dataset','time','lat','lon'])
 for model in models:
 	test2[model]=test1[model]
 test2['mod3']=data
