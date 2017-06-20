ba9cf992c7e46d3b4212491fe31891cc18449926
Peter <peterpfleiderer@gmx.de>
2017-06-20 09:47:33 +0200

    this wasn’t the issue

--- country_analysis.py
+++ country_analysis.py
@@ -138,7 +138,6 @@ class country_analysis(object):
 
 
 
-		self._adm_polygons=self.get_admin_polygons(self._working_directory+self._iso+'_adm_shp/'+self._iso+'_adm1')
 		# try to load polygons of adm regions for plotting
 		try:self._adm_polygons=self.get_admin_polygons(self._working_directory+self._iso+'_adm_shp/'+self._iso+'_adm1')
 		except:pass
