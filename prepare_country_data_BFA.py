d9e283e01ff6a7d9b14409c65b64be209fda8155
peterpeterp <peterpfleiderer@gmx.de>
2018-08-10 09:22:09 +0200

    getting to use it

--- prepare_country_data_BFA.py
+++ prepare_country_data_BFA.py
@@ -60,31 +60,5 @@ for months in [3]:
 		print rcp,model,in_file
 		COU.country_zoom(in_file,var_name='spi',given_var_name='SPI_'+str(months)+'m',data_type='CMIP5_BC',scenario=rcp,model=model,time_format='monthly',overwrite=overwrite)
 
-# #SPEI
-# for months in [1,3,6,12]:
-# 	all_files=glob.glob('/p/projects/tumble/carls/shared_folder/CMIP5_monthly/SPEI/SPEI_*'+str(months)+'m_*penman*')
-# 	for in_file in all_files:
-# 		model=in_file.split('/')[-1].split('_')[-2]
-# 		rcp=in_file.split('/')[-1].split('_')[-1].split('.')[0]
-# 		print rcp,model,in_file
-# 		COU.country_zoom(in_file,var_name='spei',given_var_name='SPEI_'+str(months)+'m_penman',data_type='CMIP5_BC',scenario=rcp,model=model,time_format='monthly',overwrite=overwrite)
-#
 
-
-COU.hist_merge()
-
-COU.ensemble_mean()
-
-COU.summary()
-
-
-COU.area_average('lat_weighted',overwrite=overwrite)
-# COU.area_average('pop2015_weighted',overwrite=False)
-# COU.area_average('pop1990_weighted',overwrite=False)
-
-
-COU.zip_it()
-
-# except Exception,e:
-# 	print str(e)
-# 	print 'issues with '+country_iso
+COU.save_data()
